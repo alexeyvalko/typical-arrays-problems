def3a19d2ea7d658290d2746067f2b3b81d24e91
alexeyvalko <valko.alexey@gmail.com>
2021-03-14 15:23:25 +0300

    feat: task is complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let min = array[0];
+        for (let item of array) {
+            min = Math.min(min, item);
+        }
+        return min;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let max = array[0];
+        for (let item of array) {
+            max = Math.max(max, item);
+        }
+        return max;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let sum = 0;
+        for (let item of array) {
+            sum += item;
+        }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+        return sum / array.length;
+    }
+};
